24fb2faf39aec741067a1ca3aa3e394aba429b24
Olga U <me@laserbat.pw>
2019-01-23 00:51:53 +0700

    Flow3: added simple lowpass filter, 3D splaying and changed the fractal resolution

--- flow3.c
+++ flow3.c
@@ -5,17 +5,20 @@
 #define MAJ_THIRD 1.126
 #define FIFTH     1.498
 
-#define COUNT 5
-#define DIV 10000
+#define COUNT 6
+#define DIV 1000
 
 uint8_t saw(uint64_t t){
     return t & 255;
 }
 
-uint8_t mix(uint64_t t){
+uint8_t mix(uint64_t t, double mul){
     double freq, scale;
-    uint64_t res = 0;
+    uint64_t res = 0, out = 0, temp = 0;
     uint64_t i, j;
+
+    uint8_t chan = t & 1;
+    t >>= 1;
 
     if(t == 0) return 0;
 
@@ -29,43 +32,55 @@ uint8_t mix(uint64_t t){
             scale /= DIV;
         }
 
-        res += saw(t * freq + res);
+
+        temp = saw(mul * t * freq + res);
+        res += temp;
+
+        if (chan == 0){
+            out += temp * ((double)i / (double)(1<<COUNT));
+        } else {
+            out += temp * (1 - ((double)i / (double)(1<<COUNT)));
+        }
+
     }
     
-    return res / (1<<COUNT);
+    return out / (1<<COUNT);
 }
 
-uint8_t chord(uint64_t t){
+uint8_t chord(uint64_t t, double mul){
     if ((t >> 20) & 1) return (
-            mix(t) +
-            mix(t * MAJ_THIRD) +
-            mix(t * FIFTH) +
-            mix(t >> 1)
+            mix(t, mul * FIFTH) +
+            mix(t, mul * MAJ_THIRD) +
+            mix(t, mul) +
+            mix(t, mul * 0.5)
         ) >> 2;
 
     return (
-            mix(t) +
-            mix(t * MIN_THIRD) +
-            mix(t * FIFTH) +
-            mix(t >> 2)
+            mix(t, mul * FIFTH) +
+            mix(t, mul * MIN_THIRD) +
+            mix(t, mul) +
+            mix(t, mul * 0.5)
         ) >> 2;
 }
 
-uint8_t octaves(uint64_t t){
+uint8_t octaves(uint64_t t, double mul){
     return (
-            chord(t >> 1) + 
-            chord(t << 0) + 
-            chord(t << 1) +
-            chord(t << 2)
+            chord(t, mul * 4) +
+            chord(t, mul * 2) +
+            chord(t, mul) + 
+            chord(t, mul * 0.5) 
         ) >> 2;
 }
 
-uint8_t stereo(uint64_t t){
-    uint8_t channel = t & 1;
-    t >>= 1;
-    
-    if (channel == 0) return octaves(t);
-    return octaves(t * 1.01);
+uint8_t lowpass(uint64_t t, double mul){
+    static uint8_t buff[4] = {0}, i;
+    uint8_t out;
+
+    buff[i] = octaves(t, mul);
+    out = (buff[i] + buff[(i + 2) % 4]) / 2;
+    i = (i + 1) % 4;
+
+    return out;
 }
 
 int main(void){
@@ -73,6 +88,6 @@ int main(void){
 
     while(1){
         t += 1;
-        putchar(octaves(t));
+        putchar(lowpass(t, 0.02));
     }
 }
