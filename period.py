64df813d6cb66265a05b2ea6daf4d71b5ab06522
Olga Ustuzhanina <me@laserbat.pw>
2019-05-04 16:07:32 +0700

    Updated license

--- period.py
+++ period.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+
+#
+# Copyright © 2019 Olga Ustiuzhanina <me@laserbat.pw>
+#
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# the COPYING file for more details.
+#
+
 from fractions import Fraction, gcd
 from math import log10
 
