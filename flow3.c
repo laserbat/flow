cfa52c4c5575099154d7656664ca4b033fbb6707
Olga Ustuzhanina <me@laserbat.pw>
2020-02-28 21:20:04 +0000

    updated flow3

--- flow3.c
+++ flow3.c
@@ -1,88 +1,85 @@
-/*
- * Copyright © 2019 Olga Ustiuzhanina <me@laserbat.pw>
- *
- * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * the COPYING file for more details.
- */
+// This file produces 44100 Hz, 2 channel, unsigned 8 bit raw PCM
+// If you're on linux you can play it using aplay:
+// $ ./flow3 | aplay -r44100 -c2
 
 #include <stdio.h>
 #include <stdint.h>
 
-// These constants can be edited freely.
-// High SYNTH_COUNT will cause high CPU load!
-#define GLOBAL_PITCH 0.02
+// I highly recommend playing with these constants as different values
+// produces different timbres and different musical development.
+
+// Be warned that high SYNTH_COUNT can make this program very slow.
 
 #define SYNTH_COUNT    128
-#define DETUNE_START   0.01
-#define DETUNE_OVERALL 1000
 
-#define STEREO_NONLIN  2
+#define GLOBAL_PITCH   0.005
+#define DETUNE_START   0.001
+#define DETUNE_STEP    100
+#define FREQ_START     0.8
 
-// Take a look at https://en.wikipedia.org/wiki/Equal_temperament#Comparison_to_just_intonation
-// before changing these constants, as they define harmonic relations between synths.
-#define OCTAVE_DOWN  0.5
-#define MIN_THIRD    1.189
-#define MAJ_THIRD    1.126
-#define FIFTH        1.498
+#define LEFT_NONLIN    2
+#define RIGHT_NONLIN  -3
+
+#define OCTAVE_DOWN    0.5
+#define MIN_THIRD      1.2
+#define MAJ_THIRD      1.125
+#define P_FIFTH        1.5
+
+double table[SYNTH_COUNT];
 
 uint8_t saw(uint64_t t){
     return t & 255;
 }
 
-uint8_t mix(uint64_t t, double mul){
-    double freq, scale;
-    double res = 0, out = 0, sum = 0;
-    double temp, coeff;
-
+void init_table(void) {
     uint16_t i, j;
-    uint8_t chan = t & 1;
-
-    t >>= 1;
 
     for (i = 0; i < SYNTH_COUNT; i ++){
-        freq = 0.5;
+        double freq, scale;
+        freq = FREQ_START;
         scale = DETUNE_START;
 
         j = i;
         while (j > 0){
             freq += (j & 1) * scale;
             j >>= 1;
-            scale /= DETUNE_OVERALL;
+            scale /= DETUNE_STEP;
         }
 
-        coeff = ((double)(i + 1) / (double)(SYNTH_COUNT));
-
-        temp = saw(mul * t * freq + res);
-        res += temp;
-
-        if (chan){
-            out += temp * coeff; 
-            res += STEREO_NONLIN;
-        } else {
-            out += temp * (1 - coeff);
-        }
-
-        sum += coeff;
+        table[i] = freq;
     }
-    
-    return (uint8_t)(out / sum + 0.5);
+}
+
+uint8_t mix(uint64_t t, double mul){
+    double freq;
+    double shift = 0, out = 0, max = 0;
+    double val, amp;
+
+    uint16_t i;
+    uint8_t chan = t & 1;
+
+    t >>= 1;
+
+    for (i = 0; i < SYNTH_COUNT; i ++){
+        freq = table[i];
+        amp = ((double)(i + 1) / (double)(SYNTH_COUNT));
+        val = saw(0.5 + mul * freq * t + shift);
+
+        shift += val;
+        max += amp;
+
+        out += val * (chan ? amp : 1 - amp);
+        shift += chan ? LEFT_NONLIN : RIGHT_NONLIN;
+    }
+
+    return (uint8_t)(out / max + 0.5);
 }
 
 uint8_t chord(uint64_t t, double mul){
-    if ((t >> 20) & 1) return (
-            mix(t, mul) +
-            mix(t, mul * FIFTH) +
-            mix(t, mul * MAJ_THIRD) +
-            mix(t, mul * OCTAVE_DOWN)
-        ) >> 2;
-
     return (
             mix(t, mul) +
-            mix(t, mul * FIFTH) +
-            mix(t, mul * MIN_THIRD) +
+            mix(t, mul * P_FIFTH) +
+            mix(t, mul * (((t >> 20) & 1) ? MAJ_THIRD : MIN_THIRD)) +
             mix(t, mul * OCTAVE_DOWN)
         ) >> 2;
 }
@@ -96,6 +93,8 @@ uint8_t octaves(uint64_t t, double mul){
 
 int main(void){
     uint64_t t = 0;
+
+    init_table();
 
     while(1){
         t += 1;
